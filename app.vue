b3fa3367cb8744168e2d4b8c78cfce606a586bd4
Nese Yildirim <neseyild@student.arteveldehs.be>
2025-05-24 13:56:12 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Welcome to My Blog</h1>
+    <p>This is a simple blog application built with Vue.js.</p>
+    <router-view></router-view>
+    <footer>
+      <p>&copy; 2025 My Blog</p>
+    </footer>  
+  </div>
+</template>
